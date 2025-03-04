a588ff4b9ed802e4ead4a612da46ee33b2bfc008
ankitmittal4 <ankitmittal0814@gmail.com>
2025-03-05 00:52:13 +0530

    Add route in userContest for update team

--- src/routes/userContest.routes.js
+++ src/routes/userContest.routes.js
@@ -3,6 +3,7 @@ import { createUserContest } from "../controllers/userContest.controller.js";
 import { getAllUserContests } from "../controllers/userContest.controller.js";
 import { getUserContestsById } from "../controllers/userContest.controller.js";
 import { updateUserContestsById } from "../controllers/userContest.controller.js";
+import { updateTeam } from "../controllers/userContest.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -15,5 +16,6 @@ router.route("/create").post(verifyJWT, createUserContest);
 router.route("/all").get(verifyJWT, getAllUserContests);
 router.route("/get").post(verifyJWT, getUserContestsById);
 router.route("/update").post(verifyJWT, updateUserContestsById);
+router.route("/update-team").post(verifyJWT, updateTeam);
 
 export default router;
