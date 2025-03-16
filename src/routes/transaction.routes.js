e060d60199406e281b0bfdc86cde741d974afd79
ankitmittal4 <ankitmittal0814@gmail.com>
2025-03-16 10:30:11 +0530

    Update transaction routes

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -4,6 +4,6 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/all").post(verifyJWT, getAllTransactions);
+router.route("/all").get(verifyJWT, getAllTransactions);
 
 export default router;
