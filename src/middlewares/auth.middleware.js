b1a99fa9ce645bc9479f46c14f1bfe974270a74f
ankitmittal4 <ankitmittal0814@gmail.com>
2025-05-15 16:20:19 +0530

    Add verifyJwt for admin

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import { Admin } from "../models/admin.model.js";
 
 const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
@@ -26,5 +27,27 @@ const verifyJWT = asyncHandler(async (req, _, next) => {
     throw new ApiError(400, "Access token not present");
   }
 });
+const verifyJWTAdmin = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.header("Authorization")?.replace("Bearer ", "") ||
+      req.cookies?.accessToken;
 
-export { verifyJWT };
+    if (!token) {
+      throw new ApiError(400, "Unauthorized Admin");
+    }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const admin = await Admin.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+    if (!admin) {
+      throw new ApiError(400, "Invalid access token");
+    }
+    req.admin = admin;
+    next();
+  } catch (error) {
+    throw new ApiError(400, "Access token not present for admin");
+  }
+});
+
+export { verifyJWT, verifyJWTAdmin };
