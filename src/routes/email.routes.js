b070461df4dbf5260ba29c15f92da58c20692521
ankitmittal4 <ankitmittal0814@gmail.com>
2025-07-08 10:36:51 +0530

    Add contact us route to email routes for handling inquiries

--- src/routes/email.routes.js
+++ src/routes/email.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { sendLoginEmail, sendContestWinEmail, sendPaymentSuccessEmail, sendPaymentFailedEmail, sendPaymentWithdrawSuccessEmail } from '../controllers/email.controller.js';
+import { sendLoginEmail, sendContestWinEmail, sendPaymentSuccessEmail, sendPaymentFailedEmail, sendPaymentWithdrawSuccessEmail, sendContactUsSuccessEmail } from '../controllers/email.controller.js';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.route("/contest-win").post(sendContestWinEmail);
 router.route("/payment-success").post(sendPaymentSuccessEmail);
 router.route("/payment-failed").post(sendPaymentFailedEmail);
 router.route("/payment-withdraw-success").post(sendPaymentWithdrawSuccessEmail);
+router.route("/contact-us").post(sendContactUsSuccessEmail);
 
 export default router;
