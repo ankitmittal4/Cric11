4897e8b657d9a8ea061ccb872e756e72cd914f8c
ankitmittal4 <ankitmittal0814@gmail.com>
2025-07-08 16:28:56 +0530

    Add send login otp and verify otp in user.routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,19 @@
 import { Router } from "express";
 import {
-  loginUser,
   logoutUser,
   registerUser,
   refreshAccessToken,
   getUserWalletBalance,
+  sendLoginOtp,
+  verifyOtpAndLoginUser
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
-router.route("/login").post(loginUser);
+router.route("/login-otp").post(sendLoginOtp);
+router.route("/verify-otp").post(verifyOtpAndLoginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/get-balance").get(verifyJWT, getUserWalletBalance);
