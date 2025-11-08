fed0ecd3d7abd7a1a12dcb04b369bee2994b988b
ankitmittal4 <ankitmittal0814@gmail.com>
2025-11-08 12:42:57 +0530

    Update send grid api key

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,16 +1,23 @@
 import nodemailer from "nodemailer";
 import { getEmailTemplate } from "./templates.js";
+import sgTransport from "nodemailer-sendgrid";
 
-const transporter = nodemailer.createTransport({
-    service: "SendGrid",
-    // service: "gmail",
-    auth: {
-        // user: process.env.EMAIL_USER,
-        // pass: process.env.EMAIL_PASS,
-        user: "apikey",
-        pass: process.env.SENDGRID_API_KEY,
-    },
-});
+// const transporter = nodemailer.createTransport({
+//     service: "SendGrid",
+//     // service: "gmail",
+//     auth: {
+//         // user: process.env.EMAIL_USER,
+//         // pass: process.env.EMAIL_PASS,
+//         user: "apikey",
+//         pass: process.env.SENDGRID_API_KEY,
+//     },
+// });
+
+const transporter = nodemailer.createTransport(
+  sgTransport({
+    apiKey: process.env.SENDGRID_API_KEY,
+  })
+);
 
 const sendEmail = async (to, type, data) => {
     const { subject, html } = getEmailTemplate(type, data);
